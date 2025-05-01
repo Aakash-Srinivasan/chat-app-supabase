619aa8db4dd13c1b92a7e3319d57957072d10a4c
Aakash Srinivasan <86234579+Aakash-Srinivasan@users.noreply.github.com>
2025-05-01 23:30:36 +0530

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './supabase';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPA_BASE_TOKEN;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPA_BASE_CHAT_ID;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPA_BASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPA_BASE_PUBLIC_ANNON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl as string, supabaseAnonKey as string, {
     auth: {
